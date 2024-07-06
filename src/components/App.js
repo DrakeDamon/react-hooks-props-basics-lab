17bca9d05d46275ecbb720609eb644ef636ac75d
Drake Damon <dddamon06@gmail.com>
2024-07-06 19:24:22 -0400

    this code will render the basics lab html

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import Links from "./Links";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
@@ -10,10 +11,10 @@ console.log(user);
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
+    <NavBar />
+    <Home color={user.color} name={user.name} city={user.city} />
+    <About bio={user.bio} links={user.links} />
+  </div>
   );
 }
 
